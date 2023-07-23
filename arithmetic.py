acf183f8b5b56dbb7376b41b596397a47de1def3
Ahzem <123859613+Ahzem@users.noreply.github.com>
2023-07-23 15:07:49 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+#arithmetic.py
+
+def add(a, b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def intdiv(a,b):
+    return a // b
+
---
+++ calc.py
@@ -0,0 +1,12 @@
+import arithmetic
+
+add = arithmetic.add(1,2)
+sub = arithmetic.sub(5,2)
+mul = arithmetic.mul(2,3)
+intdiv = arithmetic.intdiv(10,3)
+
+print(add)
+print(sub)
+print(mul)
+print(intdiv)
+
